2fc66ea5fc8cedcdaa2eba90f141727c630b58bd
ashleycarr <21591371@student.uwa.edu.au>
2016-08-08 17:07:53 +0800

    Dummy data for testing

--- lib_lifesaver.php
+++ lib_lifesaver.php
@@ -4,9 +4,31 @@ function getClosestProfessionals($lat, $lon)
 {
     // Temporary dummy dataset.
     return(array(
-        array("Freddie Fixurhead", "91 getwell drive", "5550155", -31.9812, 115.8199),
-        array("Yuri Yallrightmate", "19  lane", "5550155", -31.9812, 115.8199),
-        array("Donnie Dontbesad", "54 happyness lane", "5550155", -31.9812, 115.8199)
+        array("Freddie Fixurhead", 
+              "91 Getwell drive",
+              "5550155", 
+              -31.9812, 
+              115.7341),
+        array("Yuri Yallrightmate",
+              "19 Lovevly lane",
+              "5550155",
+              -31.8815,
+              115.7481),
+        array("Donnie Dontbesad",
+              "54 Happyness way",
+              "5550155",
+              -31.8031,
+              115.8199),
+        array("Susie Smilesalot",
+              "339 Recovery street",
+              "5550155",
+              -31.9951,
+              115.9531),
+        array("Barry Bluesaway",
+              "7 Psychiatric lane",
+              "5550155",
+              -31.8151,
+              115.8532)
     ));
 }
 
