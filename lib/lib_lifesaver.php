c70f41b2f2273576809d48333d8d62486df3936f
ashleycarr <21591371@student.uwa.edu.au>
2016-08-15 17:03:13 +0800

    lookup improvements

--- lib/lib_lifesaver.php
+++ lib/lib_lifesaver.php
@@ -9,30 +9,6 @@ function localDBConnect()
     );
 }
 
-/**
- * Haversine - using the haversine formula, calculate the distance between
- *             two latitude and longitude points
- *             http://en.wikipedia.org/wiki/Haversine_formula
- * @param  float $lat1 latitude point 1
- * @param  float $lon1 longitude point 1
- * @param  float $lat2 latitude point 2
- * @param  float $lon2 longitude point 2
- * @return float distance in km
- */
-function haversine($lat1, $lon1, $lat2, $lon2)
-{
-    $earthRadius = 6372.8; // Earth radius in meters
-    $dLat = deg2rad($lat2 - $lat1);
-    $dLon = deg2rad($lon2 - $lon1);
-    $lat1 = deg2rad($lat1);
-    $lat2 = deg2rad($lat2);
-    
-    $a = pow(sin($dLat / 2), 2) + cos($lat1) * cos($lat2) * pow(sin($dLon / 2), 2);
-    $c = 2 * asin(sqrt($a));
-    
-    return($earthRadius * $c);
-}
-
 function getClosestProfessionals($lat, $lon)
 {
     $dbh = localDBConnect();
