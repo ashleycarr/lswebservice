33c40e25dbf3a4670b4e3590692185dd4f0cf924
ashleycarr <21591371@student.uwa.edu.au>
2016-08-15 11:23:30 +0800

    fixed google geocoding for more complicated addresses

--- webservice.sql
+++ webservice.sql
@@ -5,7 +5,7 @@ CREATE TABLE `healthcare_agents` (
   `id` INT AUTO_INCREMENT,
   `name` VARCHAR(128) DEFAULT NULL,
   `address` VARCHAR(128) NOT NULL,
-  `phone` INT(11) NOT NULL,
+  `phone` VARCHAR(15) NOT NULL,
   `email` VARCHAR(254),
   `latitude` FLOAT(10, 6) NOT NULL ,
   `longitude` FLOAT(10, 6) NOT NULL ,
